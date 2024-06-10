28c94ca48f30348dbd351ccf634d97436fe56721
Chim chikwakwa <chimwec98@gmail.com>
2024-06-10 23:07:24 +0200

    nice one we have to finish the css

--- inventory/models.py
+++ inventory/models.py
@@ -20,7 +20,6 @@ UNIT_CHOICES = [
 class MenuItem(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=200)
-    description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
 
 
