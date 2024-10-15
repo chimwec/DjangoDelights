e89a225f4ae7cc43eb904f9e1fae278155721b1e
Chim chikwakwa <chimwec98@gmail.com>
2024-10-15 21:03:39 +0200

    another security

--- djangodelights/settings.py
+++ djangodelights/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-dpw5rqce91&su559!=8=vlsktiegg_=+z&r70p+d4oxxbxy3&g'
+SECRET_KEY = os.getenv ("DjangoDelight")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
